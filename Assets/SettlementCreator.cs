76ec2f4facc8ee91d02463e8c66739550ed2ad77
unknown <nagandsoft@gmail.com>
2019-02-04 17:59:45 +0100

    first commit

---
+++ Assets/SettlementCreator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nagand
+{
+    public struct TriangleProperties
+    {
+        
+        public List<GameObject> Neighbours;
+        public string Name;
+        public Vector3 Position;
+    }
+
+
+    public class SettlementCreator : MonoBehaviour
+    {
+        public GameObject RedTriangle;
+        public int HowManyTriangle;
+
+       public List<GameObject> InQueue = new List<GameObject>();
+       public List<GameObject> Finished = new List<GameObject>();
+
+
+        private void Awake()
+        {
+            
+        }
+
+        
+    }
+}
--- Assets/TileManager.cs
+++ Assets/TileManager.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public class TileManager : MonoBehaviour
 {
     public GameObject Tile;
+    public GameObject RedTriangle;
+    public float DistanceOfRoad;
+    public int HowManyTriangles;
     public int SizeX,SizeY;
     public float TileSize = 3.25f;
     public float RoadSize = 1f;
 
     float PosX=0, PosY;
 
+    
+
     private void Awake()
+    {
+        //int TrianglesInGame = 0;
+        //GameObject GO=Instantiate(RedTriangle, new Vector3(0, 0, 0), Quaternion.identity);
+        //GO.GetComponent<TriangleController>().up = Vector3.zero ;
+        //while (TrianglesInGame<HowManyTriangles)
+        //{
+
+        //}
+
+    }
+
+   
+
+    private void HexagonPlacement()
     {
         for (int i = 0; i < SizeX; i++)
         {
@@ -19,14 +38,14 @@ public class TileManager : MonoBehaviour
             {
                 PosY = 0;
             }
-            else PosY = TileSize / 2+ RoadSize;
+            else PosY = TileSize / 2 + RoadSize;
             for (int j = 0; j < SizeY; j++)
             {
                 Vector3 tempPos = new Vector3(PosX, PosY, 0);
                 Instantiate(Tile, tempPos, Quaternion.identity);
-                PosY += TileSize+RoadSize*2;
+                PosY += TileSize + RoadSize * 2;
             }
-            PosX += TileSize +RoadSize;
+            PosX += TileSize + RoadSize;
         }
     }
 }
