2a6cf1de5ec04057184ca2d60a8c590ddd2b0fb2
unknown <nagandsoft@gmail.com>
2019-02-05 20:48:28 +0100

    first commit

--- Assets/GameStarter.cs
+++ Assets/GameStarter.cs
@@ -9,21 +9,36 @@ namespace Nagand
         public GameObject StartingTile;
         HexagonController script;
         public HexagonController[] lista ;
+        public List<HexagonController> NotAllNeighbourIsSet = new List<HexagonController>();
+        public int HowManyIteration;
         void Start()
         {
-            for (int i = 0; i < 250; i++)
+            for (int i = 0; i < HowManyIteration; i++)
             {
                 CreateHexagons();
             }
-           
+            foreach (HexagonController con in StartingTile.GetComponentsInChildren<HexagonController>())
+            {
+                con.SetUpRoads();
+            }
+            foreach (HexagonController con in StartingTile.GetComponentsInChildren<HexagonController>())
+            {
+                con.ConnectAllTilesTogether();
+            }
         }
 
         void CreateHexagons()
         {
             lista = StartingTile.GetComponentsInChildren<HexagonController>();
-            int rnd = UnityEngine.Random.Range(0, lista.Length);
-            lista[rnd].PopulateNeighbours();
-           
+            for (int i = 0; i < lista.Length; i++)
+            {
+                if (!lista[i].IsFullyNeighboured())
+                    NotAllNeighbourIsSet.Add(lista[i]);
+            }
+
+            int rnd = UnityEngine.Random.Range(0, NotAllNeighbourIsSet.Count);
+            NotAllNeighbourIsSet[rnd].PopulateNeighbours();
+            NotAllNeighbourIsSet.Clear();
         }
 
     }
--- Assets/HexagonController.cs
+++ Assets/HexagonController.cs
@@ -12,14 +12,17 @@ namespace Nagand
     {
         public GameObject prefab;
         public GameObject[] NeighbouringTiles = new GameObject[6];
+        public GameObject[] Roads=new GameObject[6];
         public GameObject parentObject;
+        public GameObject RoadPrefab;
         public int iteration = 0;
         public static int currentIteration=0;
 
-        
+
 
         public void PopulateNeighbours()
         {
+           
             prefab = Resources.Load<GameObject>("PREFAB_GrassTILE1.0fSide");
             Debug.Log(prefab);
             currentIteration++;
@@ -31,8 +34,8 @@ namespace Nagand
                 NeighbouringTiles[(int)TileDirections.West].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.East] = gameObject;
                 NeighbouringTiles[(int)TileDirections.West].GetComponent<HexagonController>().parentObject = parentObject;
                 NeighbouringTiles[(int)TileDirections.West].transform.parent = parentObject.transform;
-                NeighbouringTiles[(int)TileDirections.West].GetComponent<HexagonController>().iteration= currentIteration;
-            }
+                NeighbouringTiles[(int)TileDirections.West].GetComponent<HexagonController>().iteration= currentIteration;               
+           }
            
             #endregion
             #region NorthWest Handling
@@ -96,41 +99,139 @@ namespace Nagand
 
 
             #endregion
-            #region Connect All
+            ConnectAllTilesTogether();
+
+
+
+}
+        public void SetUpRoads()
+        {
+            RoadPrefab = Resources.Load<GameObject>("PREFAB_Road");
+            if (Roads[(int)TileDirections.West] == null)
+            {
+                Roads[(int)TileDirections.West] = Instantiate(RoadPrefab, transform.position - new Vector3(1.015f, 0, 0), Quaternion.identity);
+                Roads[(int)TileDirections.West].transform.Rotate(0, 0, 90);
+                if (NeighbouringTiles[(int)TileDirections.West]!=null)
+                NeighbouringTiles[(int)TileDirections.West].GetComponent<HexagonController>().Roads[(int)TileDirections.East] = Roads[(int)TileDirections.West];
+            }
+            if (Roads[(int)TileDirections.NorthWest] == null)
+            {
+                Roads[(int)TileDirections.NorthWest] = Instantiate(RoadPrefab, transform.position - new Vector3(.5f, -.866f, 0), Quaternion.identity);
+                Roads[(int)TileDirections.NorthWest].transform.Rotate(0, 0, 30);
+                if (NeighbouringTiles[(int)TileDirections.NorthWest] != null)
+                    NeighbouringTiles[(int)TileDirections.NorthWest].GetComponent<HexagonController>().Roads[(int)TileDirections.SouthEast] = Roads[(int)TileDirections.NorthWest];
+            }
+            if (Roads[(int)TileDirections.NorthEast] == null)
+            {
+                Roads[(int)TileDirections.NorthEast] = Instantiate(RoadPrefab, transform.position + new Vector3(.5f, .866f, 0), Quaternion.identity);
+                Roads[(int)TileDirections.NorthEast].transform.Rotate(0, 0, -30);
+                if (NeighbouringTiles[(int)TileDirections.NorthEast] != null)
+                    NeighbouringTiles[(int)TileDirections.NorthEast].GetComponent<HexagonController>().Roads[(int)TileDirections.SouthWest] = Roads[(int)TileDirections.NorthEast];
+            }
+            if (Roads[(int)TileDirections.East] == null)
+            {
+                Roads[(int)TileDirections.East] = Instantiate(RoadPrefab, transform.position + new Vector3(1.015f, 0, 0), Quaternion.identity);
+                Roads[(int)TileDirections.East].transform.Rotate(0, 0, 90);
+                if (NeighbouringTiles[(int)TileDirections.East] != null)
+                    NeighbouringTiles[(int)TileDirections.East].GetComponent<HexagonController>().Roads[(int)TileDirections.West] = Roads[(int)TileDirections.East];
+            }
+            if (Roads[(int)TileDirections.SouthEast] == null)
+            {
+                Roads[(int)TileDirections.SouthEast] = Instantiate(RoadPrefab, transform.position + new Vector3(.5f, -.866f, 0), Quaternion.identity);
+                Roads[(int)TileDirections.SouthEast].transform.Rotate(0, 0, 30);
+                if (NeighbouringTiles[(int)TileDirections.SouthEast] != null)
+                    NeighbouringTiles[(int)TileDirections.SouthEast].GetComponent<HexagonController>().Roads[(int)TileDirections.NorthWest] = Roads[(int)TileDirections.SouthEast];
+            }
+            if (Roads[(int)TileDirections.SouthWest] == null)
+            {
+                Roads[(int)TileDirections.SouthWest] = Instantiate(RoadPrefab, transform.position + new Vector3(-.5f, -.866f, 0), Quaternion.identity);
+                Roads[(int)TileDirections.SouthWest].transform.Rotate(0, 0, -30);
+                if (NeighbouringTiles[(int)TileDirections.SouthWest] != null)
+                    NeighbouringTiles[(int)TileDirections.SouthWest].GetComponent<HexagonController>().Roads[(int)TileDirections.NorthEast] = Roads[(int)TileDirections.SouthWest];
+            }
+        }
+
+        public void ConnectAllTilesTogether()
+        {
             //Connecting West to NW and SW
-            if (NeighbouringTiles[(int)TileDirections.NorthWest] != null)
+            if (NeighbouringTiles[(int)TileDirections.NorthWest] != null&& NeighbouringTiles[(int)TileDirections.West]!=null)
             {
                 NeighbouringTiles[(int)TileDirections.West].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.NorthEast] = NeighbouringTiles[(int)TileDirections.NorthWest];
                 NeighbouringTiles[(int)TileDirections.NorthWest].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.SouthWest] = NeighbouringTiles[(int)TileDirections.West];
             }
-            if (NeighbouringTiles[(int)TileDirections.SouthWest] != null)
+            if (NeighbouringTiles[(int)TileDirections.SouthWest] != null && NeighbouringTiles[(int)TileDirections.West] != null)
             {
                 NeighbouringTiles[(int)TileDirections.West].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.SouthEast] = NeighbouringTiles[(int)TileDirections.SouthWest];
                 NeighbouringTiles[(int)TileDirections.SouthWest].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.NorthWest] = NeighbouringTiles[(int)TileDirections.West];
             }
+            //Connecting NorthWest to W and NE
+            if (NeighbouringTiles[(int)TileDirections.West] != null && NeighbouringTiles[(int)TileDirections.NorthWest] != null)
+            {
+                NeighbouringTiles[(int)TileDirections.West].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.NorthEast] = NeighbouringTiles[(int)TileDirections.NorthWest];
+                NeighbouringTiles[(int)TileDirections.NorthWest].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.SouthWest] = NeighbouringTiles[(int)TileDirections.West];
+            }
+            if (NeighbouringTiles[(int)TileDirections.NorthEast] != null && NeighbouringTiles[(int)TileDirections.NorthWest] != null)
+            {
+                NeighbouringTiles[(int)TileDirections.NorthEast].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.West] = NeighbouringTiles[(int)TileDirections.NorthWest];
+                NeighbouringTiles[(int)TileDirections.NorthWest].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.East] = NeighbouringTiles[(int)TileDirections.NorthEast];
+            }
             //Connecting NorthEast to NW and E
-            if (NeighbouringTiles[(int)TileDirections.East] != null)
+            if (NeighbouringTiles[(int)TileDirections.East] != null && NeighbouringTiles[(int)TileDirections.NorthEast] != null)
             {
                 NeighbouringTiles[(int)TileDirections.NorthEast].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.SouthEast] = NeighbouringTiles[(int)TileDirections.East];
                 NeighbouringTiles[(int)TileDirections.East].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.NorthWest] = NeighbouringTiles[(int)TileDirections.NorthEast];
             }
-            if (NeighbouringTiles[(int)TileDirections.NorthEast] != null)
+            if (NeighbouringTiles[(int)TileDirections.NorthWest] != null && NeighbouringTiles[(int)TileDirections.NorthEast] != null)
             {
                 NeighbouringTiles[(int)TileDirections.NorthWest].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.East] = NeighbouringTiles[(int)TileDirections.NorthEast];
                 NeighbouringTiles[(int)TileDirections.NorthEast].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.West] = NeighbouringTiles[(int)TileDirections.NorthWest];
             }
+            //Connecting East to NE and SE
+            if (NeighbouringTiles[(int)TileDirections.NorthEast] != null && NeighbouringTiles[(int)TileDirections.East] != null)
+            {
+                NeighbouringTiles[(int)TileDirections.NorthEast].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.SouthEast] = NeighbouringTiles[(int)TileDirections.East];
+                NeighbouringTiles[(int)TileDirections.East].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.NorthWest] = NeighbouringTiles[(int)TileDirections.NorthEast];
+            }
+            if (NeighbouringTiles[(int)TileDirections.SouthEast] != null && NeighbouringTiles[(int)TileDirections.East] != null)
+            {
+                NeighbouringTiles[(int)TileDirections.SouthEast].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.NorthEast] = NeighbouringTiles[(int)TileDirections.East];
+                NeighbouringTiles[(int)TileDirections.East].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.SouthWest] = NeighbouringTiles[(int)TileDirections.SouthEast];
+            }
             //Connecting SouthEast to E and SW
-            if (NeighbouringTiles[(int)TileDirections.SouthWest] != null)
+            if (NeighbouringTiles[(int)TileDirections.SouthWest] != null && NeighbouringTiles[(int)TileDirections.SouthEast] != null)
             {
                 NeighbouringTiles[(int)TileDirections.SouthEast].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.West] = NeighbouringTiles[(int)TileDirections.SouthWest];
                 NeighbouringTiles[(int)TileDirections.SouthWest].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.East] = NeighbouringTiles[(int)TileDirections.SouthEast];
             }
-            if (NeighbouringTiles[(int)TileDirections.East] != null)
+            if (NeighbouringTiles[(int)TileDirections.East] != null && NeighbouringTiles[(int)TileDirections.SouthEast] != null)
             {
                 NeighbouringTiles[(int)TileDirections.East].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.SouthWest] = NeighbouringTiles[(int)TileDirections.SouthEast];
                 NeighbouringTiles[(int)TileDirections.SouthEast].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.NorthEast] = NeighbouringTiles[(int)TileDirections.East];
             }
-                #endregion
+            //Connecting SouthWest to W and SE
+            if (NeighbouringTiles[(int)TileDirections.SouthEast] != null && NeighbouringTiles[(int)TileDirections.SouthWest] != null)
+            {
+                NeighbouringTiles[(int)TileDirections.SouthEast].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.West] = NeighbouringTiles[(int)TileDirections.SouthWest];
+                NeighbouringTiles[(int)TileDirections.SouthWest].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.East] = NeighbouringTiles[(int)TileDirections.SouthEast];
+            }
+            if (NeighbouringTiles[(int)TileDirections.West] != null && NeighbouringTiles[(int)TileDirections.SouthWest] != null)
+            {
+                NeighbouringTiles[(int)TileDirections.West].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.SouthEast] = NeighbouringTiles[(int)TileDirections.SouthWest];
+                NeighbouringTiles[(int)TileDirections.SouthWest].GetComponent<HexagonController>().NeighbouringTiles[(int)TileDirections.NorthWest] = NeighbouringTiles[(int)TileDirections.West];
+            }
+        }
+        public bool IsFullyNeighboured()
+        {
+            bool result = true;
+            for (int i = 0; i < NeighbouringTiles.Length; i++)
+            {
+                if (NeighbouringTiles[i]==null)
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
         }
     }
 }
